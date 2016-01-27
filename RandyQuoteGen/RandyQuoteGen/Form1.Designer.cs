cd2c40a020562684cfc704e64727b0310ccb45e7
Hurro <kurtisbowen99@gmail.com>
2016-01-27 10:22:44 -0600

    Google Drive

---
+++ RandyQuoteGen/RandyQuoteGen/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RandyQuoteGen
+{
+    partial class frm_randyQuoteGenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Randy = new System.Windows.Forms.Button();
+            this.pic_quotebox = new System.Windows.Forms.PictureBox();
+            this.pic_Background = new System.Windows.Forms.PictureBox();
+            this.lbl_Num = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_quotebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Randy
+            // 
+            this.btn_Randy.Location = new System.Drawing.Point(12, 486);
+            this.btn_Randy.Name = "btn_Randy";
+            this.btn_Randy.Size = new System.Drawing.Size(535, 63);
+            this.btn_Randy.TabIndex = 3;
+            this.btn_Randy.Text = "Random Internet Quote";
+            this.btn_Randy.UseVisualStyleBackColor = true;
+            this.btn_Randy.Click += new System.EventHandler(this.btn_Randy_Click);
+            // 
+            // pic_quotebox
+            // 
+            this.pic_quotebox.BackColor = System.Drawing.Color.DimGray;
+            this.pic_quotebox.Image = global::RandyQuoteGen.Properties.Resources.cooltext1721520660;
+            this.pic_quotebox.Location = new System.Drawing.Point(12, 60);
+            this.pic_quotebox.Name = "pic_quotebox";
+            this.pic_quotebox.Size = new System.Drawing.Size(535, 341);
+            this.pic_quotebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_quotebox.TabIndex = 4;
+            this.pic_quotebox.TabStop = false;
+            this.pic_quotebox.Click += new System.EventHandler(this.pic_quotebox_Click);
+            // 
+            // pic_Background
+            // 
+            this.pic_Background.Image = global::RandyQuoteGen.Properties.Resources.congruent_outline;
+            this.pic_Background.Location = new System.Drawing.Point(-3, -2);
+            this.pic_Background.Name = "pic_Background";
+            this.pic_Background.Size = new System.Drawing.Size(564, 566);
+            this.pic_Background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_Background.TabIndex = 0;
+            this.pic_Background.TabStop = false;
+            this.pic_Background.Click += new System.EventHandler(this.pic_Background_Click);
+            // 
+            // lbl_Num
+            // 
+            this.lbl_Num.AutoSize = true;
+            this.lbl_Num.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Num.Location = new System.Drawing.Point(12, 377);
+            this.lbl_Num.Name = "lbl_Num";
+            this.lbl_Num.Size = new System.Drawing.Size(20, 24);
+            this.lbl_Num.TabIndex = 5;
+            this.lbl_Num.Text = "0";
+            // 
+            // frm_randyQuoteGenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 561);
+            this.Controls.Add(this.lbl_Num);
+            this.Controls.Add(this.pic_quotebox);
+            this.Controls.Add(this.btn_Randy);
+            this.Controls.Add(this.pic_Background);
+            this.Name = "frm_randyQuoteGenForm";
+            this.Text = "Random Picture Quote Gen";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_quotebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Background)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_Background;
+        private System.Windows.Forms.Button btn_Randy;
+        private System.Windows.Forms.PictureBox pic_quotebox;
+        private System.Windows.Forms.Label lbl_Num;
+    }
+}
+
---
+++ RandyQuoteGen/RandyQuoteGen/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandyQuoteGen
+{
+    public partial class frm_randyQuoteGenForm : Form
+    {
+        //random Number Generator Used to randomize Quotes
+        Random rndGen = new Random();
+        public frm_randyQuoteGenForm()
+        {
+            //Start Code
+            InitializeComponent();
+            // do not put "Random rndGen = new Random();" here, it wont work
+        }
+
+        private void pic_Background_Click(object sender, EventArgs e)
+        {
+            //Accidental double click 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Accidental double click, again
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //Accidental double click, yet again
+        }
+
+        private void btn_Randy_Click(object sender, EventArgs e)
+        {
+            //generate random number using generator above
+            int rndNum = rndGen.Next(1, 11);
+            //place image (with qoute) based on number generated
+            if (rndNum == 1)
+            {
+                pic_quotebox.Image = Properties.Resources.Ana_White_Quote_Inspirational_Quotes;
+               lbl_Num.Text = "1";
+            }
+            else if (rndNum == 2)
+            {
+                pic_quotebox.Image = Properties.Resources.patience;
+                lbl_Num.Text = "2";
+            }
+            else if (rndNum == 3)
+            {
+                pic_quotebox.Image = Properties.Resources.googleit;
+                lbl_Num.Text = "3";
+            }
+            else if (rndNum == 4)
+            {
+                pic_quotebox.Image = Properties.Resources.images;
+                lbl_Num.Text = "4";
+            }
+            else if (rndNum == 5)
+            {
+                pic_quotebox.Image = Properties.Resources.Quotation_Marc_Andreessen_magic_internet_Meetville_Quotes_2427;
+                lbl_Num.Text = "5";
+            }
+            else if (rndNum == 6)
+            {
+                pic_quotebox.Image = Properties.Resources.images__1_;
+                lbl_Num.Text = "6";
+            }
+            else if (rndNum == 7)
+            {
+                pic_quotebox.Image = Properties.Resources.Quotation_Kent_Conrad_society_computers_education_internet_Meetville_Quotes_43535;
+                lbl_Num.Text = "7";
+            }
+            else if (rndNum == 8)
+            {
+                pic_quotebox.Image = Properties.Resources.Quotation_D_M_Jarrett_media_listening_time_Meetville_Quotes_177522;
+                lbl_Num.Text = "8";
+            }
+            else if (rndNum == 9)
+            {
+                pic_quotebox.Image = Properties.Resources.Quotation_Michio_Kaku_wisdom_trying_people_ideas_internet_Meetville_Quotes_3513;
+                lbl_Num.Text = "9";
+            }
+            else if (rndNum == 10)
+            {
+                pic_quotebox.Image = Properties.Resources.Quotation_Andrew_Brown_life_people_technology_internet_Meetville_Quotes_188562;
+                lbl_Num.Text = "10";
+            }
+            else
+            {
+                //Just in case...
+                pic_quotebox.Image = Properties.Resources.cooltext1721567769;
+            }
+        }
+
+        private void pic_quotebox_Click(object sender, EventArgs e)
+        {
+            //Accidental double click, again
+        }
+    }
+}
